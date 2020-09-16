1aab83947271e07db892ad8a0e5fdbefc2dbab75
louisjdcharles <53097306+louisjdcharles@users.noreply.github.com>
2020-09-16 20:03:42 +0100

    added css capabilities

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,17 @@ const path = require('path');
 module.exports = {
     watch: true,
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
-        }]
+        ]
     },
     output: {
         filename: 'bundle.js',
