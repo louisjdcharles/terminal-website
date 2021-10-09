5cc1609cbeaa55f5a2399ee110789ce4d4d6c58d
louisjdcharles <53097306+louisjdcharles@users.noreply.github.com>
2021-10-09 16:00:49 +0100

    nuked!

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-
-import "./index.css"
-
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
